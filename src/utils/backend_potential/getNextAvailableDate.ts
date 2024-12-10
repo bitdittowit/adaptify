8d4c0514103d95bd3708ab452df58932ce8c59f1
dboyarinkov <dboyarinkov@yandex-team.ru>
2024-12-10 19:52:13 +0300

    minor refactor

--- src/utils/backend_potential/getNextAvailableDate.ts
+++ src/utils/backend_potential/getNextAvailableDate.ts
@@ -8,20 +8,16 @@ export default function getNextAvailableDate(
 ): Date {
   for (let i = 0; i < 7; i++) {
     const dateToCheck = addDays(currentDate, i);
-    console.log('dateToCheck', dateToCheck)
     const dayOfWeek = daysOfWeek[dateToCheck.getDay()];
-    console.log('dayOfWeek', dayOfWeek)
 
     if (schedule[dayOfWeek] && schedule[dayOfWeek].length > 0) {
       for (const timeRange of schedule[dayOfWeek]) {
-        const [startTime, endTime] = timeRange.split('-');
-        console.log('startTime', startTime)
+        const [startTime] = timeRange.split('-');
         const startDateTime = parse(
           `${format(dateToCheck, 'yyyy-MM-dd')} ${startTime}`,
           'yyyy-MM-dd HH:mm',
           new Date(),
         );
-        console.log('startDateTime', startDateTime)
 
         if (isAfter(startDateTime, currentDate)) {
           return startDateTime;
