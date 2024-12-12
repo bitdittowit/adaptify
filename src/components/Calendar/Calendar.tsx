f32efde126d96d96189bad9858e092c216f98ca2
Tim <tsxell@yahoo.com>
2024-12-12 20:46:58 +0300

    wip

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,44 +2,42 @@
 import { useState } from "react";
 import { Calendar as CalendarUI } from "@/components/ui/calendar"
 import { ru } from "date-fns/locale/ru";
+import Holidays from 'date-holidays'
 
 
+const holidays = new Holidays('RU');
 
 
-const Calendar = ({}) => {
-    const [date, setDate] = useState<Date | undefined>(undefined)
- 
-return (
-  <CalendarUI
-    mode="single"
-    selected={date}
-    today={new Date()}
-    onSelect={setDate}
-    className="rounded-md border"
-    showOutsideDays
-    weekStartsOn={1}
-    locale={ru}
-    modifiers={{
-        weekend: {dayOfWeek: [0, 6]}
-    }}
-    modifiersClassNames={{
-        weekend: "day_weekend"
-      }}
-    onDayClick={(date, modifiers) => {
-        if (modifiers.weekend) {
-          alert("This day is already booked.");
-        }
-      }}
-      classNames={{
-        day_weekend: "bg-red-500 text-white"
-      }}
-    // footer={
-    //     selected
-    //       ? `You picked ${selected.toLocaleDateString()}.`
-    //       : "Please pick a date."
-    //   }
-  />
-)
+const Calendar = ({ }) => {
+    const [date, setDate] = useState<Date | undefined>(new Date())
+
+    return (
+        <CalendarUI
+            mode="single"
+            selected={date}
+            today={new Date()}
+            onSelect={setDate}
+            className="rounded-md border"
+            showOutsideDays={true}
+            weekStartsOn={1}
+            locale={ru}
+            modifiers={{
+                weekend: (day: Date) => day.getDay() === 0 || day.getDay() === 6 || Boolean(holidays.isHoliday(day)),
+            }}
+            modifiersClassNames={{
+                weekend: "text-red-500",
+            }}
+            classNames={{
+                day_today: "border-2 border-zinc-950",
+                day_outside: "text-gray-200",
+            }}
+        // footer={
+        //     selected
+        //       ? `You picked ${selected.toLocaleDateString()}.`
+        //       : "Please pick a date."
+        //   }
+        />
+    )
 }
 
 export default Calendar;
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -50,7 +50,7 @@ function Calendar({
         day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-pink-700 text-primary-foreground hover:bg-pink-300",
+        // day_today: "bg-pink-700 text-primary-foreground hover:bg-pink-300",
         day_outside:
           "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
