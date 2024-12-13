df413da7ead798a25e2652c0c50200d033e1ad8f
Tim <tsxell@yahoo.com>
2024-12-13 03:43:08 +0300

    working

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,41 +1,59 @@
 'use client'
-import { useState } from "react";
-import { Calendar as CalendarUI } from "@/components/ui/calendar"
+import mockData from '@/app/constants/tasks/user_tasks.json';
+import { Calendar as BaseCalendar } from "@/components/ui/calendar";
+import { Status, type BaseTask, type Task } from "@/types";
+import { getRandomDateInMonth, getRandomInRange, getTasksForDay } from '@/utils/calendarUtils';
+import { isPast, isSameMonth, isToday, isWeekend } from 'date-fns';
 import { ru } from "date-fns/locale/ru";
-import Holidays from 'date-holidays'
-
+import Holidays from 'date-holidays';
+import { useState } from "react";
+import { type DayProps as BaseDayProps, type FooterProps as BaseFooterProps } from "react-day-picker";
+import { Day } from './Day/Day';
+import { Footer } from './Footer/Footer';
 
 const holidays = new Holidays('RU');
-
+const mockTasks = (mockData.tasks as BaseTask[])
+        .map((task) => ({
+            ...task,
+            status: Status.OPEN,
+            picked_date: getRandomDateInMonth(),
+            experience_points: getRandomInRange(1, 10),
+        } satisfies Task))
+        .filter(({ picked_date }) => 
+            !(isWeekend(picked_date) || Boolean(holidays.isHoliday(picked_date)))
+        );
 
 const Calendar = ({ }) => {
     const [date, setDate] = useState<Date | undefined>(new Date())
 
+    const predicates = {
+        past: (day: Date) => !isToday(day) && isPast(day) && isSameMonth(day, date || new Date()),
+        weekend: (day: Date) => !isPast(day) && isSameMonth(day, date || new Date()) && (isWeekend(day) || Boolean(holidays.isHoliday(day))),
+    }
+
     return (
-        <CalendarUI
+        <BaseCalendar
+            components={{
+                Day: (props: BaseDayProps) => <Day {...props} tasks={getTasksForDay(mockTasks, props.date)} />,
+                Footer: (props: BaseFooterProps) => <Footer {...props} tasks={getTasksForDay(mockTasks, date || new Date())} />
+            }}
             mode="single"
             selected={date}
             today={new Date()}
             onSelect={setDate}
+            onMonthChange={setDate}
             className="rounded-md border"
             showOutsideDays={true}
             weekStartsOn={1}
             locale={ru}
-            modifiers={{
-                weekend: (day: Date) => day.getDay() === 0 || day.getDay() === 6 || Boolean(holidays.isHoliday(day)),
-            }}
+            disabled={
+                (day: Date) => (predicates.past(day) || !isSameMonth(day, date || new Date()))
+            }
+            modifiers={predicates}
             modifiersClassNames={{
                 weekend: "text-red-500",
+                past: "text-gray-400",
             }}
-            classNames={{
-                day_today: "border-2 border-zinc-950",
-                day_outside: "text-gray-200",
-            }}
-        // footer={
-        //     selected
-        //       ? `You picked ${selected.toLocaleDateString()}.`
-        //       : "Please pick a date."
-        //   }
         />
     )
 }
---
+++ src/components/Calendar/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import type { DayOfWeek, Task } from "@/types";
+import { getDay } from "date-fns";
+import { type FooterProps as BaseFooterProps } from "react-day-picker";
+import styles from './Footer.module.css';
+
+type FooterProps = BaseFooterProps & {
+    tasks: Pick<Task, 'title' | 'id' | 'schedule'>[];
+}
+
+const getDayOfWeek = (date: Date): DayOfWeek => {
+    const days: DayOfWeek[] = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    return days[getDay(date)];
+};
+
+export const Footer = ({ tasks, displayMonth }: FooterProps) => {
+    return (
+        <tfoot className={styles.footer}>
+            {tasks?.map(({ title, id, schedule }) => (
+                <tr key={id}>
+                    <td key={id}>{title}: <b>{schedule?.[getDayOfWeek(displayMonth || new Date())]}</b></td>
+                </tr>
+            ))}
+        </tfoot>
+    )
+}
